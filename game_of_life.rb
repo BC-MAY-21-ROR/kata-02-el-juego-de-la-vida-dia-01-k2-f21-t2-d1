84ff263e9d6e2e6148e10ce6b1bf772642ce4000
Francisco Comparán Pantoja <78762927+Frcomparan@users.noreply.github.com>
2022-02-16 19:07:37 -0600

    Complete feature 1, 2 and 3
    Co-Authored-By: Franklin Huichi Contreras <54047442+FranH20@users.noreply.github.com>
    Co-Authored-By: Javier Ruiz <42743118+Javi1499@users.noreply.github.com>
    Co-Authored-By: brocha05 <87162540+brocha05@users.noreply.github.com>

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,63 +1,70 @@
-# Primero ingresar datos
-# text = <<TEXT
-# ........
-# ....*...
-# ...**...
-# ........
 def create_grid(row, column)
-    matriz = {
-        'row' => row,
-        'column' => column
-    }
-    grid= %w{. *}
-    grid_template = Hash.new
-    matriz['row'].times do |r|
-        # puts "#{grid[rand(0..1)] * matriz['column']}"
-        grid_template[r] =[] 
-        matriz['column'].times do |c|
-            random = rand(0..100)
-            if random > 80
-                grid_template[r] << grid[1]
-                # grid_template += grid[1]
-            else
-                grid_template[r] << grid[0]
-                # grid_template += grid[0]
-            end
-        end
+  matriz = {
+    'row' => row,
+    'column' => column
+  }
+  grid = ['.', '*']
+  grid_template = {}
+  matriz['row'].times do |r|
+    grid_template[r] = []
+    matriz['column'].times do
+      random = rand(0..100)
+      grid_template[r] << (random > 80 ? grid[1] : grid[0])
     end
-    grid_template
+  end
+  grid_template
 end
 
-def show_template (grid)
-    grid.values.each { |line|
-        puts line.join
-    }
+
+def show_template(grid)
+  grid.each_value { |line| puts line.join }
+  puts ""
 end
 
 def analyze_grid(grid)
-    coord = {
-        row: [],
-        column:[]
-    }
-    grid.values.each.with_index() do |line,r|
-        line.each.with_index() do |ch, c|
-            if ch == "*"
-                coord[:row] << r
-                coord[:column] << c
-            end
-        end
+  coord = { row: [], column: [] }
+  grid.values.each.with_index do |line, r|
+    line.each.with_index do |ch, c|
+      if ch == '*'
+        coord[:row] << r
+        coord[:column] << c
+      end
     end
-    puts coord.inspect
+  end
+  coord
 end
 
-grid = create_grid(4,8)
+def count_neighbors(coord, grid)
+  grid_copy = Marshal.load(Marshal.dump(grid))
+  neighbors = 0
+  coord[:row].each.with_index do |item, i|
+    row = coord[:row][i]
+    column = coord[:column][i]
+      (neighbors += 1 if (grid[row - 1][column - 1] == '*')) if (row > 0 && column > 0)
+      (neighbors += 1 if (grid[row - 1][column] == '*')) if (row > 0)
+      (neighbors += 1 if (grid[row - 1][column + 1] == '*')) if (row > 0 && column < grid[0].length - 1)
+      (neighbors += 1 if (grid[row][column - 1] == '*')) if (column > 0)
+      (neighbors += 1 if (grid[row][column + 1] == '*')) if (column < grid[0].length-1)
+      (neighbors += 1 if (grid[row + 1][column - 1] == '*')) if (row < grid.length-1 && column > 0)
+      (neighbors += 1 if (grid[row + 1][column + 1] == '*')) if (row < grid.length-1 && column < grid[0].length-1)
+      (neighbors += 1 if (grid[row + 1][column] == '*')) if (row < grid.length-1)
+      isLive = live_or_die(neighbors)
+      grid_copy[row][column] = (isLive ? '*' : '.')      
+    print ("#{neighbors}, ")
+    neighbors = 0
+  end
+  show_template(grid_copy)
+end
+
+def live_or_die(neighbors)
+  (neighbors == 2 || neighbors == 3)
+  
+end
+
+
+
+grid = create_grid(4, 8)
 
 show_template(grid)
-analyze_grid(grid)
 
-# TEXT
-#Co-authored-by: brocha05 <brocha_05@hotmail.com>
-# <2 vecinos muere
-# >3 vecinos muere
-# == 2 || == 3 vecinos vive en la siguiente generación
-# 
+count_neighbors(analyze_grid(grid), grid)
